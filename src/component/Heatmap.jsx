2846fb6a01abd7309a50df2f3d4bea03d43bddf4
Joseph Qin <Joseph.Qin@agios.com>
2017-10-31 18:25:00 -0400

    test2

--- src/component/Heatmap.jsx
+++ src/component/Heatmap.jsx
@@ -1,8 +1,32 @@
+import React from 'react'
+import * as utility from '../utility'
+
+const Node = function(props){
+	const d = props.data.parent ? utility.drawLine({x:d.y,y:d.x},{x:d.parent.y,y:d.x}):'' + (props.data.children && d.children && d.children.length > 1 )? utility.drawLine({x:d.y,y:d.children[0].x},{x:d.y,y:d.children[1].x})
+	return (
+		<g>
+			<path d={d}/>
+		</g>
+		); 
+}
+
+
+
+const Dendrogram = function(props){
+    
+    const nodes = data.descendants().slice(1).map((node,index)=>(<Node data={d} />)
+    
+	return (<g>{ nodes }</g>)
+}
 
 
 
 const Heatmap = function(props){
-
-   return (<Dendrogram width={props.width} height={props.hight} data={props.dataset.rows}/>);
+   const dendrogramData = utility.dendrogram(props.rows,props.)
+   return ( 
+   			<svg width={props.width} height={props.hight}>
+   	        	<Dendrogram data={dendrogramData}/>
+   	        <svg/>
+   	       );
 
 }
--- src/utility.js
+++ src/utility.js
@@ -4,7 +4,16 @@ import * as d3 from 'd3';
 export var tranSlate = (x,y)=>'translate('+x+','+y+')';
 export var drawCircle = (radius) => 'M '+(0-radius)+' '+0+' a '+radius+' '+radius+', 0, 1, 0, '+(radius*2)+' '+0+' '+'a '+radius+' '+radius+', 0, 1, 0, '+(-radius*2)+' '+0;
 export var drawRect = (width,height) => 'M'+(0-width/2)+','+(0-height/2)+' h '+width+' v '+height+' h '+(0-width)+' Z ';
-export var drawLine = (range,direction) => 'M 0,0 '+direction+' '+Math.abs(range[1]-range[0]);
+export var drawLine = (sourse,target) => 'M'+sourse.x+','+sourse.y+' L '+target.x+','+target.y;
+
 
 export var pagination = (oldRange,boundry,step) => (oldRange.length<boundry && oldRange[0]+step >= 0 && oldRange[oldRange.length-1]+step <= boundry)?oldRange.map((d)=>d+step):oldRange ;
 export var createArray = d3.range;
+
+//dendrogram
+export var dendrogram = function(data,width,height){
+	let root = d3.hierarchy(data);
+    let cluster = d3.cluster().size([width,height]).separation((a,b)=>a.parent == b.parent?1:1)
+    cluster(root);
+    return root
+}
